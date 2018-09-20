3375a88cf9770b9ca7e7a377211ee9827efc565e
zhiyong.qiao <zhiyong.qiao@finlabtech.com>
2018-09-20 17:07:39 +0800

    11

---
+++ src/main/java/com/example/ribbon/RibbonApplication.java
@@ -0,0 +1,37 @@
+package com.example.ribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class RibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/hello")
+    public String hello(String name){
+        String result=restTemplate.getForObject("http://HELLOSERVICE/hello?name="+name,String.class);
+        return result;
+
+    }
+}
