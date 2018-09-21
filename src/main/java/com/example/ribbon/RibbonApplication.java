8ccc5bbb4b600124da0d5046af33096f884df1db
zhiyong.qiao <zhiyong.qiao@finlabtech.com>
2018-09-21 11:10:16 +0800

    qq

--- src/main/java/com/example/ribbon/RibbonApplication.java
+++ src/main/java/com/example/ribbon/RibbonApplication.java
@@ -1,10 +1,12 @@
 package com.example.ribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@EnableHystrix
 public class RibbonApplication {
 
     public static void main(String[] args) {
@@ -29,10 +32,14 @@ public class RibbonApplication {
     private RestTemplate restTemplate;
 
     @RequestMapping("/hello")
+    @HystrixCommand(fallbackMethod = "helloError")
     public String hello(String name){
         //服务名不区分大小写
         String result=restTemplate.getForObject("http://helloservice/hello?name="+name,String.class);
         return result;
 
+    }
+    public String helloError(String name){
+        return "hello  ,"+name+"!  sorry ,error !";
     }
 }
